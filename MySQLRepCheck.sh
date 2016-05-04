d8f8a6754b22853b30436eb0f01b932584208a21
natsidan4 <gary.williams@emishealth.com>
2016-05-04 23:06:10 +0100

    Added files via upload

---
+++ MySQLRepCheck.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#script to check Mysql replication is running
+
+     primary=`mysql --user=emisuser --password=T3ster -e "show slave status\G" | grep -i " Master_Log_File:" | awk '{print $2}'`
+    if [[ $primary != *"mysql-bin"* ]]
+then
+   exit 1
+fi
+    status=0
+    MasterHost=`ssh emisuser@crm-mysql-rep.emis.local hostname`
+    SlaveHost="$HOSTNAME"
+    emails="Gary.Williams@emishealth.com Mark.Burn@emishealth.com Frazer.Bradburn@emishealth.com " #multiple emails space separated
+    DownSubject="Replication status - Down"
+    GoodSubject="Replication status - Good"
+    GoodMessage="MySQL replication on $MasterHost and $SlaveHost is good"
+    #Grab the lines for each and use awk to get the last part of the string(Yes/No)
+    SQLresponse=`mysql --user=emisuser --password=T3ster  emis_sugarcrm -e "show slave status \G" |grep -i "Slave_SQL_Running"| awk '{print $2}'`
+    IOresponse=`mysql  --user=emisuser --password=T3ster  emis_sugarcrm -e "show slave status \G" |grep -i "Slave_IO_Running" | awk '{print $2}'`
+    if [ "$SQLresponse" = "No" ]; then
+    error="Replication on the slave MySQL server($SlaveHost) has stopped working to ($MasterHost). Slave_SQL_Running not running:"
+    status=1
+    fi
+    if [ "$IOresponse" = "No" ]; then
+    error="Replication on the slave MySQL server($SlaveHost) has stopped working to ($MasterHost). Slave_IO_Running not running:"
+    status=1
+    fi
+    # If the replication is not working
+    if [ $status = 1 ]; then
+    for address in $emails; do
+    echo $error | mutt -s "$DownSubject" $address
+    echo "Replication down, sent email to $address"
+    done
+    fi
+
+    #If the replication is working fine
+    #if [ $status = 0 ]; then
+    #for address in $emails; do
+    #echo  $GoodMessage | mutt -s "$GoodSubject" $address
+    #echo "Replication is up, still sent email to $address"
+    #done
+    #fi
